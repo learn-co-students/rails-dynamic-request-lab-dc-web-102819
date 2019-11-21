667aa03280f3a23549ac358cc17fa7876273f03b
Kyle <kylepcole@gmail.com>
2019-11-21 15:17:10 -0500

    lab completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+kyle = Student.create(first_name: "Kyle", last_name: "Cole")
+john = Student.create(first_name: "John", last_name: "Doe")
+steve = Student.create(first_name: "Steve", last_name: "Smith")
+cory = Student.create(first_name: "Cory", last_name: "Wong")
+
